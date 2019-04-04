fcc271209dcdeac5e2dcbfc04bfd79f7d2e31ba1
Paul Michel <pmichel31415@gmail.com>
2019-04-04 10:13:41 -0400

    Increment version number

--- teapot/__init__.py
+++ teapot/__init__.py
@@ -2,7 +2,7 @@ from teapot.scorers import Scorer, register_scorer
 from teapot.scorers import BLEU, METEOR, ChrF, ZeroOne
 
 
-__version__ = "0.2"
+__version__ = "0.2.1"
 
 __all__ = [
     "Scorer",
